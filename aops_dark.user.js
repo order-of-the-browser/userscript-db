57501b799bc18a4434167e170805a945ee2c4f78
Raymond <20248577+javaarchive@users.noreply.github.com>
2020-04-11 21:57:15 -0700

    Create aops_dark.user.js

---
+++ aops_dark.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         AoPS Dark Side
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.aopsacademy.org/*
+// @match        https://artofproblemsolving.com/*
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @require      https://darkmodejs.learn.uno/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(function(){const darkmode = new Darkmode(options);
+darkmode.showWidget();
+});
+    // Your code here...
+})();
